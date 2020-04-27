7556fa2584f500c79a4ee425695704e3cc90378d
PhucIT <lehuuphuc23031999@gmail.com>
2020-04-27 21:25:36 +0700

    Updates

---
+++ precache-manifest.6c5a8ee059e8e04b5c6f6120d37523d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fd50fd631bc7809351bcae4deb6f962",
+    "url": "/todo-list-reactjs/index.html"
+  },
+  {
+    "revision": "90202d16ee86e057a617",
+    "url": "/todo-list-reactjs/static/css/main.59ce2411.chunk.css"
+  },
+  {
+    "revision": "6a56aaf36f63bb102644",
+    "url": "/todo-list-reactjs/static/js/2.47919b4b.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/todo-list-reactjs/static/js/2.47919b4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90202d16ee86e057a617",
+    "url": "/todo-list-reactjs/static/js/main.3c158020.chunk.js"
+  },
+  {
+    "revision": "feded381cd80f60b5736",
+    "url": "/todo-list-reactjs/static/js/runtime-main.be2aea90.js"
+  }
+]);
